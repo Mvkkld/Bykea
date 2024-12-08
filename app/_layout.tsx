4a1d7c7ae69026ffd9662b477e7810df98cba9e1
Mvkkld <mvk40kld@gmail.com>
2024-12-08 21:56:03 +0500

    change for test

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -188,6 +188,7 @@ export default function RootLayout() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
